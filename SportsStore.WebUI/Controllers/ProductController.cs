40c8894a708e1bb6d7847da483a13ca72e4c57dd
Jesse M <gangreen@gmail.com>
2013-02-14 17:35:59 -0800

    Chapter 8
    At the beginning of Chapter 8

--- SportsStore.WebUI/Controllers/ProductController.cs
+++ SportsStore.WebUI/Controllers/ProductController.cs
@@ -15,23 +15,27 @@ namespace SportsStore.WebUI.Controllers
         public int PageSize = 4;
 
         public ProductController(IProductRepository productRepository)
-            {
-                this.repository = productRepository;
-            }
-        public ViewResult List(int page = 1)
+        {
+            this.repository = productRepository;
+        }
+        public ViewResult List(string category, int page = 1)
         {
             ProductsListViewModel model = new ProductsListViewModel
             {
                 Products = repository.Products
-                .OrderBy(p => p.ProductID)
-                .Skip((page - 1) * PageSize)
-                .Take(PageSize),
+                    .Where(p => category == null || p.Category == category)
+                    .OrderBy(p => p.ProductID)
+                    .Skip((page - 1) * PageSize)
+                    .Take(PageSize),
                 PagingInfo = new PagingInfo
                     {
                         CurrentPage = page,
                         ItemsPerPage = PageSize,
-                        TotalItems = repository.Products.Count()
-                    }
+                        TotalItems = category == null ?
+                            repository.Products.Count() :
+                            repository.Products.Where(e => e.Category == category).Count()
+                    },
+                    CurrentCategory = category
             };
             return View(model);
         }
